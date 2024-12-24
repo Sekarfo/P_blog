1fd4023490b6b9a2026b654a2359ec08a2858a94
trudk <trudkhanov@inbox.ru>
2024-12-24 11:47:22 +0500

    1 vers

--- main.go
+++ main.go
@@ -3,117 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 	"log"
+	"net/http"
 )
 
 type Response struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
-}
-
-type User struct {
-	ID    uint   `gorm:"primaryKey"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-func migrateDB() {
-	err := db.AutoMigrate(&User{})
-	if err != nil {
-		log.Fatal("Failed to migrate database:", err)
-	}
-
-	fmt.Println("Database migrated successfully!")
-}
-
-var db *gorm.DB
-
-func initDB() {
-
-	dsn := "host=localhost user=postgres password=0000 dbname=postgres port=5433 sslmode=disable TimeZone=Asia/Almaty"
-	var err error
-
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to the database:", err)
-	}
-
-	fmt.Println("Database connected successfully!")
-}
-
-func createUser(w http.ResponseWriter, r *http.Request) {
-	var user User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-
-	result := db.Create(&user)
-	if result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(user)
-}
-
-func getUserByID(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var user User
-
-	result := db.First(&user, params["id"])
-	if result.Error != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	json.NewEncoder(w).Encode(user)
-}
-
-func updateUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var user User
-
-	result := db.First(&user, params["id"])
-	if result.Error != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-
-	db.Save(&user)
-	json.NewEncoder(w).Encode(user)
-}
-
-func deleteUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	result := db.Delete(&User{}, params["id"])
-
-	if result.Error != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
-}
-
-func getAllUsers(w http.ResponseWriter, r *http.Request) {
-	var users []User
-	db.Find(&users)
-
-	json.NewEncoder(w).Encode(users)
 }
 
 func handleRequests(w http.ResponseWriter, r *http.Request) {
@@ -127,7 +23,7 @@ func handleRequests(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusBadRequest)
 			json.NewEncoder(w).Encode(Response{
 				Status:  "fail",
-				Message: "Invalid JSON message",
+				Message: "Invalid message empty",
 			})
 			return
 		}
@@ -168,24 +64,11 @@ func handleRequests(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	initDB()
-	migrateDB()
-	router := mux.NewRouter()
-
-	router.HandleFunc("/users", createUser).Methods("POST")
-	router.HandleFunc("/users", getAllUsers).Methods("GET")
-	router.HandleFunc("/users/{id}", getUserByID).Methods("GET")
-	router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
-	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
-
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./")))
-
 	http.HandleFunc("/", handleRequests)
 	log.Println("Server is running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", router)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
 
-	log.Fatal(http.ListenAndServe(":8080", router))
 }
